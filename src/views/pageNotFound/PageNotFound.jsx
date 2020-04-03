e78d14e63788fc707972ff769d067fc254f8b6e2
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-04-03 23:56:46 +0100

    created 404 page

---
+++ src/views/pageNotFound/PageNotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Alert from "../../components/Alert"
+
+// import './PageNotFound.css';
+
+function PageNotFound() {
+  return (
+    <div className="PageNotFound">
+     <Alert/>
+    </div>
+  );
+}
+
+export default PageNotFound;
