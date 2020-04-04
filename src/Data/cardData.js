f6a105a489901563f3a492c8b523fc2bf1af5ea7
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-04-04 15:04:42 +0100

    created card dummy data

---
+++ src/Data/cardData.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Hackers",
+    img: "assets/person-computer.jpeg",
+    text:
+      "Learn and grow as a tech professional. Gain access to learning resources, Hackathons and more",
+  },
+  {
+    title: "Enablers",
+    img: "assets/group.jpeg",
+    text:
+      "Employers, recuiters, HR Personal of organisations, and the likes can get the best candidates for Jobs",
+  },
+  {
+    title: "Advocates",
+    img: "assets/lady-book.jpeg",
+    text:
+      "Providing resources through tools, products or services",
+  },
+];
